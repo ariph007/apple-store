761fc69e6bf1ff7c2a7ab1ca9df8107fca8a2c5c
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-09-27 00:24:11 +0700

    Added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,81 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Tab } from "@headlessui/react";
+import type { GetServerSideProps } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Landing from "../components/Landing";
+import Product from "../components/Product";
+import { fetchCategories } from "../utils/fetchCategories";
+import { fetchProducts } from "../utils/fetchProducts";
 
-const Home: NextPage = () => {
+interface homeProps {
+  categories: Category[];
+  products: Product[];
+}
+
+const Home = ({ categories, products }: homeProps) => {
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Product product={product} key={product._id} />);
+  };
+
   return (
     <div className="">
       <Head>
         <title>Apple Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>Lets build Apple Redesign</h1>
+      <Header />
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
+        <Landing />
+      </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <div className="space-y-10 py-16">
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+            New Promos
+          </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {categories?.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light
+              outline-none transition duration-200 md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#35383C] text-white"
+                  : "text-[747474] border-b-2 border-[#35383C]"
+              }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+//Backend code
+export const getServerSideProps: GetServerSideProps<homeProps> = async () => {
+  const categories = await fetchCategories();
+  const products = await fetchProducts();
+  return {
+    props: {
+      categories,
+      products,
+    },
+  };
+};
